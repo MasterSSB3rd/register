a32b66c810b1b55e52107c5db297f357413d2fcb
MasterSSB3rd <elmoutaouakkilyns@gmail.com>
2026-09-18 23:33:50 +0100

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -6,7 +6,7 @@
 // Paste the Web App URL you get after deploying apps-script/Code.gs
 // (Deploy > New deployment > Web app). It looks like:
 // https://script.google.com/macros/s/AKfycb.../exec
-const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxi4fk9f4yD5UuAXzbWvk12ugwP0V3YGE5nOewG3JHWpjh_Sc2t0ZN26jhF6i3V3FG6/exec";
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyprMOIUJ2x6fy_Ek3xDrs5VnBryXN63doZm125dxH7VwmmfDj7XGIFsMVahSfcuxD5yg/exec";
 
 // "Niveau d'étude" dropdown options, in the order they appear.
 const NIVEAU_OPTIONS = [
